0d929ebb7e09b761971b602df856ee60268ef0e6
Hugo <trevisani.hugo@gmail.com>
2020-05-18 17:03:00 +0200

    add collisions settings

---
+++ CollisionSettings.cpp
@@ -0,0 +1,50 @@
+//
+// Created by lucas on 16/05/2020.
+//
+
+#include <cmath>
+#include <iostream>
+#include "CollisionSettings.h"
+
+double CollisionSettings::getE() const {
+    return e;
+}
+
+void CollisionSettings::setE(double e) {
+    CollisionSettings::e = e;
+}
+
+double CollisionSettings::getMu() const {
+    return mu;
+}
+
+void CollisionSettings::setMu(double mu) {
+    CollisionSettings::mu = mu;
+}
+
+double CollisionSettings::getKn() const {
+    return kn;
+}
+
+void CollisionSettings::setKn(double kn) {
+    CollisionSettings::kn = kn;
+}
+
+double CollisionSettings::getKt() const {
+    return kt;
+}
+
+void CollisionSettings::setKt(double kt) {
+    CollisionSettings::kt = kt;
+}
+
+CollisionSettings::CollisionSettings(double e, double mu, double kn, double kt) : e(e), mu(mu), kn(kn), kt(kt) {
+    CollisionSettings::setE(e);
+    CollisionSettings::setMu(mu);
+    CollisionSettings::setKn(kn);
+    CollisionSettings::setKt(kt);
+}
+
+double CollisionSettings::getEta(double effectiveMass) {
+    return -2. * log(e) * sqrt(effectiveMass * kn / (pow(log(e), 2) + pow(M_PI, 2)));
+}
---
+++ CollisionSettings.h
@@ -0,0 +1,38 @@
+//
+// Created by lucas on 16/05/2020.
+//
+
+#ifndef TRAVAIL2_COLLISIONSETTINGS_H
+#define TRAVAIL2_COLLISIONSETTINGS_H
+
+
+class CollisionSettings {
+private:
+    double e = 0.99;
+    double mu = 0.1;
+    double kn = 200.;
+    double kt = 100000.;
+public:
+    CollisionSettings(double e, double mu, double kn, double kt);
+
+    double getE() const;
+
+    void setE(double e);
+
+    double getMu() const;
+
+    void setMu(double mu);
+
+    double getKn() const;
+
+    void setKn(double kn);
+
+    double getKt() const;
+
+    void setKt(double kt);
+
+    double getEta(double effectiveMass);
+};
+
+
+#endif //TRAVAIL2_COLLISIONSETTINGS_H
